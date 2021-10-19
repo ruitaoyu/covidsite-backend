ea497a4552ab646a647d44f407044547b337fd7f
Ruitao Yu <44585641+ruitaoyu@users.noreply.github.com>
2021-10-19 13:27:59 -0700

    init

---
+++ covidsite-backend/src/main/java/net/covidsite/backend/CovidsiteBackendApplication.java
@@ -0,0 +1,13 @@
+package net.covidsite.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidsiteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidsiteBackendApplication.class, args);
+	}
+
+}
